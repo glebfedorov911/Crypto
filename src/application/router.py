298fcb37c869c2e42a4562323b90d1a957bed6bb
Gleb <fedorov22134@gmail.com>
2025-06-27 13:48:24 +0300

    add routs for applications

---
+++ src/application/router.py
@@ -0,0 +1,95 @@
+from typing import List
+from datetime import datetime
+
+from fastapi import APIRouter, Query
+
+from src.application.schemas import (
+    Status,
+    SellerApplication, PayApplication
+)
+
+
+router = APIRouter(prefix="/api/v1/application", tags=["application"])
+
+@router.get("/statuses/")
+async def statuses() -> Status:
+    return Status(
+        accept=[
+            "new",
+            "coldnew",
+            "coldpay",
+            "verify",
+            "payed",
+            "realpay",
+            "success",
+            "coldsuccess",
+            "error",
+            "cancel",
+            "delete",
+        ],
+        pay=[
+            "new",
+            "coldnew",
+            "coldpay",
+            "verify",
+            "payed",
+            "realpay",
+            "success",
+            "coldsuccess",
+            "error",
+            "cancel",
+            "delete"
+        ]
+    )
+
+@router.get("/applications/")
+async def applications(filters: List[str] = Query(None)) -> List[PayApplication]:
+    return [
+        PayApplication(
+        id_application=74098,
+        currency="Рубль",
+        seller=[
+            SellerApplication(
+                bank="Liberty Bank",
+                sum=999.77,
+                currency="USD-T",
+                date=datetime.today(),
+                requisites="12345678900987654321",
+            ),
+            SellerApplication(
+                bank="JSC Isbank Georgia",
+                sum=412.12,
+                currency="USD-T",
+                date=datetime.today(),
+                requisites="13579086422468097531",
+            ),
+        ],
+        bank="Сбербанк",
+        sum=120000.0,
+        date=datetime.today(),
+        ),
+
+        PayApplication(
+            id_application=74099,
+            currency="Доллар",
+            seller=[
+                SellerApplication(
+                    bank="Т-Банк",
+                    sum=6543,
+                    currency="Рубль",
+                    date=datetime.today(),
+                    requisites="123456654321178900987",
+                ),
+                SellerApplication(
+                    bank="ВТБ",
+                    sum=3457,
+                    currency="Рубль",
+                    date=datetime.today(),
+                    requisites="54546363727291910808",
+                ),
+            ],
+            bank="JSC Isbank Georgia",
+            sum=125.0,
+            date=datetime.today(),
+        )
+    ]
